9dab292b1ef34ce0d969d0027ad4dbc72cb408b8
Abdullah Binsalman <A.binsalman03@Gmail.com>
2024-11-06 23:46:46 +0300

    Moving Average Trader + Huge changes

--- MovingAverageTrader.java
+++ MovingAverageTrader.java
@@ -1,50 +1,52 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class MovingAverageTrader extends Trader implements knowledgeableTrader{
 
     private int period;
-    private Queue<Double> values;
+//    private Queue<Double> values;
 
-    public MovingAverageTrader(int period) {
+    public MovingAverageTrader(String name, double cash, int period) {
+        super(name, cash);
         this.period = period;
-        this.values = new LinkedList<>();
+//        this.values = new LinkedList<>();
 
     }
    
-    public double calculate(int period) {
-        if (values.size() < period) {
+    public double calculate(int period, List<Double> priceHistory) {
+        if (priceHistory.size() < period) {
             // إذا كانت عدد القيم أقل من الفترة المطلوبة، لا يمكننا حساب المتوسط
             return 0.0;
         }
 
         double sum = 0.0;
-        for (double value : values) {
+        for (double value : priceHistory) {
             sum += value;
         }
         
          return sum / period;
     }
 
-   public void execute() {
-        double movingAverage = calculate();
-        double currentPrice = values.peek() != null ? values.peek() : 0.0;
+   public void execute(Stocks stock) {
+        List<Double> priceHistory = stock.getPriceHistory();
+        double movingAverage = calculate(this.period, priceHistory);
+        double currentPrice = priceHistory.size() != 0.0 ? priceHistory.get(priceHistory.size() - 1) : 0.0;
 
         if (currentPrice > movingAverage) {
             System.out.println("Action: Sell stock, price is above the moving average.");
         } else {
             System.out.println("Action: Buy stock, price is below or equal to the moving average.");
         }
-    
-    // دالة لإضافة قيمة جديدة وتحديث القائمة
-    public void addValue(double newValue) {
-        if (values.size() == period) {
-            values.poll(); // إزالة أقدم قيمة إذا كانت القائمة ممتلئة
-        }
-        values.add(newValue); // إضافة القيمة الجديدة
-    }
-
-    
-    public String getName() { return  "MovingAverage Trading Strategy";}
 }
 
+    public String getName() { return "MovingAverage Trading Strategy";}
+}
+
+    // دالة لإضافة قيمة جديدة وتحديث القائمة
+//    public void addValue(double newValue) {
+//        if (values.size() == period) {
+//            values.poll(); // إزالة أقدم قيمة إذا كانت القائمة ممتلئة
+//        }
+//        values.add(newValue); // إضافة القيمة الجديدة
+//    }

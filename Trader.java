732a8dc015e86f13937ec7a1cd6544e92fae2659
Abdullah Binsalman <a.binsalman03@Gmail.com>
2024-11-08 21:11:53 +0300

    Changes in Trader.java

--- Trader.java
+++ Trader.java
@@ -3,10 +3,10 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public class Trader {
+public abstract class Trader {
     private String name;
     private double cash;
-    private HashMap<String, Integer> stockPortfolio;
+    private HashMap<Stocks, Integer> stockPortfolio;
     private double netWorth;
     private ArrayList<Double> worthHistory;
 
@@ -17,55 +17,56 @@ public class Trader {
         this.worthHistory = new ArrayList<>();
     }
 
-    public void buy(String stock, int quantity, double price) {
+    public boolean buy(Stocks stock, int quantity, double price) {
         double totalCost = price * quantity;
         if (cash >= totalCost) {
             cash -= totalCost;
             stockPortfolio.put(stock, stockPortfolio.getOrDefault(stock, 0) + quantity);
+            return true;
         } else {
             System.out.println("Not enough cash to buy " + quantity + " of " + stock);
+            return false;
         }
     }
 
-    public void sell(String stock, int quantity, double price) {
+    public boolean sell(Stocks stock, int quantity, double price) {
         if (stockPortfolio.containsKey(stock) && stockPortfolio.get(stock) >= quantity) {
             double totalRevenue = price * quantity;
             cash += totalRevenue;
             stockPortfolio.put(stock, stockPortfolio.get(stock) - quantity);
+            
             if (stockPortfolio.get(stock) == 0) {
                 stockPortfolio.remove(stock);
             }
+            return true;
+
         } else {
             System.out.println("Not enough stock to sell " + quantity + " of " + stock);
+            return false;       
         }
     }
 
-    public double calculateNetWorth() {
+    public double calculateNetWorth(HashMap<Stocks, Integer> stockPortfolio) {
         netWorth = cash;
-        for (String stock : stockPortfolio.keySet()) {
-            double stockPrice = getStockPrice(stock);
+        for (Stocks stock : stockPortfolio.keySet()) {
+            double stockPrice = stock.getPrice();
             netWorth += stockPrice * stockPortfolio.get(stock);
         }
         worthHistory.add(netWorth);
         return netWorth;
     }
 
-    public void trade(String stock, int quantity, double price, boolean isBuying) {
-        if (isBuying) {
-            buy(stock, quantity, price);
-        } else {
-            sell(stock, quantity, price);
-        }
-    }
+    // public void trade(String stock, int quantity, double price, boolean isBuying) {
+    //     if (isBuying) {
+    //         buy(stock, quantity, price);
+    //     } else {
+    //         sell(stock, quantity, price);
+    //     }
+    // }
 
-    public double calculate() {
-        return calculateNetWorth();
-    }
+    public abstract double calculate();
 
-    public void execute(String stock, int quantity, double price, boolean isBuying) {
-        trade(stock, quantity, price, isBuying);
-        System.out.println("Executed " + (isBuying ? "buy" : "sell") + " for " + quantity + " of " + stock + " at price " + price);
-    }
+    public abstract void execute();
 
     public String getName() {
         return name;

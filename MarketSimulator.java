bbe1ab7a3c996b7811955ea4e4c851914cbed936
Abdullah Binsalman <a.binsalman03@Gmail.com>
2024-11-10 01:55:44 +0300

    MarketSimulator.java methods

--- MarketSimulator.java
+++ MarketSimulator.java
@@ -1,45 +1,50 @@
-import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MarketSimulator {
-    private ArrayList<Stocks> listStock;
-    private ArrayList<Event> listEvent;
+    private static ArrayList<Stocks> listStock;
+    private static ArrayList<Event> listEvent;
     private static final String EVENT_CSV = "events.csv";
     private static final String STOCK_CSV = "stocks.csv";
 
     public MarketSimulator() {
         listStock = new ArrayList<>();
         listEvent = new ArrayList<>();
-        loadStock();
+        loadFiles(EVENT_CSV);
+        loadFiles(STOCK_CSV);
     }
 
 
-    public void loadStock(){
-        try (BufferedReader reader = new BufferedReader(new FileReader(STOCK_CSV))) {
+    public static void loadFiles(String path){
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            
             String line;
             while ((line = reader.readLine()) != null) {
-
+                
                 String[] parts = line.split(",");
+
+                if (parts.length == 3) {
                 listStock.add(new Stocks(parts[0], parts[1], Double.parseDouble(parts[2])));
+            }
+                else {
+                    listEvent.add(new Event(parts[0], Double.parseDouble(parts[1])));
+                }
 
             }
-
+            
         } catch (IOException e) {
-            System.out.println("Error reading stock file: " + e.getMessage());
+            System.out.println("Error reading file: " + e.getMessage());
         }
-
+        
     }
 
-    public void intializeEvent(String name, double impact){
-        Event event = new Event(name, impact);
-        listEvent.add(event);
-    }
-     public void applyEventToStock(Stocks stock, Event event){
+    
+    public void applyEventToStock(Stocks stock, Event event){
         double random = Math.random();
-        double priceChange = event.getImpact() *(event.getImpact()+random);
+        double priceChange = stock.getPrice() *(event.getImpact()+random);
         stock.setPrice(stock.getPrice() + priceChange);
      }
 

bf6e14cc0d8f138fc3900a3c9836c8c74e073936
Ahmed Alharthy <AhmedAlharthy017@gmail.com>
2024-11-10 01:27:07 +0300

    add load methods for stocks.csv

--- MarketSimulator.java
+++ MarketSimulator.java
@@ -1,12 +1,36 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MarketSimulator {
     private ArrayList<Stocks> listStock;
     private ArrayList<Event> listEvent;
+    private static final String EVENT_CSV = "events.csv";
+    private static final String STOCK_CSV = "stocks.csv";
 
     public MarketSimulator() {
         listStock = new ArrayList<>();
         listEvent = new ArrayList<>();
+        loadStock();
+    }
+
+
+    public void loadStock(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(STOCK_CSV))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(",");
+                listStock.add(new Stocks(parts[0], parts[1], Double.parseDouble(parts[2])));
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading stock file: " + e.getMessage());
+        }
+
     }
 
     public void intializeEvent(String name, double impact){

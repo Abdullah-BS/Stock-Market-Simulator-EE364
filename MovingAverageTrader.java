85135eaa347a1b215ebcd7dec1e1c49a34a5effe
Abdullah-BS <139412761+Abdullah-BS@users.noreply.github.com>
2024-12-09 22:06:08 +0300

    Advice Vs Action MA

--- MovingAverageTrader.java
+++ MovingAverageTrader.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MovingAverageTrader extends Trader implements knowledgeableTrader {
@@ -31,34 +32,51 @@ public class MovingAverageTrader extends Trader implements knowledgeableTrader {
         List<Double> priceHistory = stock.getPriceHistory();
         double movingAverage = calculate(this.period, priceHistory);
         double currentPrice = stock.getPrice();
+        String advice;
+        String action;
 
+        // Determine the advice based on market conditions
+        if (currentPrice > movingAverage * (1 + threshold)) {
+            advice = "Sell";
+        } else if (currentPrice < movingAverage * (1 - threshold)) {
+            advice = "Buy";
+        } else {
+            advice = "Hold";
+        }
+
+        // Simulate trader's action (random excuse or actual action)
         if (random < 0.3) {
             System.out.println(randomExcuses());
+            action = randomExcuses();
         } else {
-
-            //SELL
-            if (currentPrice > movingAverage * (1 + threshold) && getStockPortfolio().containsKey(stock)) {
+            // SELL
+            if (advice.equals("Sell") && getStockPortfolio().containsKey(stock)) {
                 do {
                     if (getStockPortfolio().get(stock) >= quantity) {
                         sell(stock, quantity, currentPrice);
-                        System.out.println(this.getName() + ":Sold " + quantity + " units of " + stock.getSymbol() +
+
+                        action = "Sold " + quantity + " units of " + stock.getSymbol() +
+                                " at price " + currentPrice;
+                        System.out.println(this.getName() + ": Sold " + quantity + " units of " + stock.getSymbol() +
                                 " at price " + currentPrice);
                         break;
                     }
                     quantity--;
 
                     if (quantity <= 0) {
-                        System.out.println(this.getName() + ":Not enough stock to sell.");
+                        System.out.println("Not enough stock to sell.");
+                        action = "Not enough stock to sell";
                         break;
                     }
                 } while (true);
             }
-
-            //BUY
-            else if (currentPrice < movingAverage * (1 - threshold)) {
+            // BUY
+            else if (advice.equals("Buy")) {
                 do {
                     if (getCash() >= quantity * currentPrice) {
                         buy(stock, quantity, currentPrice);
+                        action = "Bought " + quantity + " units of " + stock.getSymbol() +
+                                " at price " + currentPrice;
                         System.out.println(this.getName() + ": Bought " + quantity + " units of " + stock.getSymbol() +
                                 " at price " + currentPrice);
                         break;
@@ -68,17 +86,22 @@ public class MovingAverageTrader extends Trader implements knowledgeableTrader {
 
                     if (quantity <= 0) {
                         System.out.println(this.getName() + ": Not enough cash to buy stock " + stock.getSymbol());
+                        action = "Not enough cash to buy stock " + stock.getSymbol();
                         break;
                     }
                 } while (true);
             } else {
+                action = "Hold";
                 System.out.println(this.getName() + ": Hold stock, price is within the threshold range of the moving average.");
             }
-
-
-
         }
+
+        getAdvice_VS_action().put(advice, action);
+
     }
+
+
+
     public String getName() {
         return super.getName() + " (Moving Average)";
     }

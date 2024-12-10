0845dc94f47cd26d2370b3a186a4a701a47b671c
SaudDuas <83941535+SaudDuas@users.noreply.github.com>
2024-12-10 21:40:33 +0300

    SUPPOSED TO BE NO EXCEPTION RN

--- MovingAverageTrader.java
+++ MovingAverageTrader.java
@@ -4,8 +4,7 @@ public class MovingAverageTrader extends Trader implements knowledgeableTrader {
 
     private int period;
     private double threshold = 0.015;
-    private static final double STOP_LOSS_PERCENTAGE = 0.10; // 10% stop-loss
-    private static final double PROFIT_GRAB_PERCENTAGE = 0.45; // 45% profit-grab
+ 
     private static final int MAX_TRADES_PER_DAY = 2; // Max trades allowed per day
 
     public MovingAverageTrader(String name, int period, MarketSimulator market) {
@@ -151,34 +150,6 @@ public class MovingAverageTrader extends Trader implements knowledgeableTrader {
                 sellAction = "Hold";
             }
         }
-
-//        // Stop-loss and profit-grab checks for all owned stocks
-//        for (Map.Entry<Stocks, Integer> entry : new HashMap<>(getStockPortfolio()).entrySet()) {
-//            if (dailyTradeCount >= MAX_TRADES_PER_DAY) break;
-//
-//            Stocks portfolioStock = entry.getKey();
-//            int ownedQuantity = entry.getValue();
-//            double purchasePrice = portfolioStock.getPriceHistory().get(portfolioStock.getPriceHistory().size() - 1); // Last price as purchase price
-//            double profitPercentage = (currentPrice - purchasePrice) / purchasePrice;
-//
-////             Stop-loss logic
-//            if (profitPercentage <= -STOP_LOSS_PERCENTAGE) {
-//                sell(portfolioStock, ownedQuantity, currentPrice);
-//                dailyTradeCount++;
-//                System.out.println(this.getName() + ": Sold (Stop Loss) " + ownedQuantity + " units of " +
-//                        portfolioStock.getSymbol() + " at price " + currentPrice);
-//                return; // Exit after executing stop-loss
-//            }
-//
-//            // Profit-grab logic
-//            if (profitPercentage >= PROFIT_GRAB_PERCENTAGE) {
-//                sell(portfolioStock, ownedQuantity, currentPrice);
-//                dailyTradeCount++;
-//                System.out.println(this.getName() + ": Sold (Profit Grab) " + ownedQuantity + " units of " +
-//                        portfolioStock.getSymbol() + " at price " + currentPrice);
-//                return; // Exit after executing profit-grab
-//            }
-//        }
 
 
         getBuy_Advice_VS_action().put(buyAdviceMessage, buyAction);
